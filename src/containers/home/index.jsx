494fd963026a5cb4b845611cb69f15ce4c3c7061
cartapro <cartakun@gmail.com>
2021-03-24 17:42:07 -0500

    se añadieron los recursos para cada vista

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,18 @@
+import './style.css'
+
+function Home() {
+    return(
+        <div className="contenido">
+            <div className="contentLeft">
+                <div className="contentLeftTop">
+                    
+                </div>
+            </div>
+            <div className="contentRight"></div>
+
+
+        </div>
+    );
+
+}
+export default Home;
